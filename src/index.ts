1f78b84f14df00bd652733210a6bb79a02983eac
Gauthier Rodaro <gauthier.rodaro@gmail.com>
2019-09-12 23:02:53 +0200

    feat: add book endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,16 @@ import { bookWod } from "./wodify/bookWod"
 const app = express()
 const port = process.env.PORT || 8080
 
-// define a route handler for the default home page
 app.get("/", (req, res) => {
   res.send("Wodify booker!")
 })
 
-// start the Express server
+app.get("/book", async (req, res) => {
+  const twoWeeksFromNow = moment().add(14, "days")
+  await bookWod(twoWeeksFromNow)
+  res.sendStatus(200)
+})
+
 app.listen(port, () => {
   // Runs from Monday to Thursday at 8h35 belgium time (utc-2)
   scheduleJob("0 35 10 * * 1-4", async () => {
